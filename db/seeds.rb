2f361db55a5f3c3f766df0e4636094a90db9d946
omozgovoi <omozgovoi@voiplatinum.com>
2017-11-07 18:53:35 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Todo.create!(title: 'Auto deploy ova to VirtualBox', notes: 'Firs step')
+Todo.create!(title: 'Create App')
+Todo.create!(title: 'Add some fitures', notes: 'Need some new awesome fitures')
+
